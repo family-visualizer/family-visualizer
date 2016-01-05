7c7d3347fd1e87e991e5366ad6d0b7993c50b1ab
Erin Meyers <erinmeyers88@gmail.com>
2016-01-05 09:45:48 -0700

    Fixed auth and added test route.

--- main/client/app.js
+++ main/client/app.js
@@ -1,1 +1,20 @@
-angular.module('app', ['ui.router']);
+angular.module('app', ['ui.router']).config(['$stateProvider', '$urlRouterProvider', routing]);
+
+function routing($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider
+        .otherwise('/');
+
+
+    $stateProvider
+        .state('chart1', {
+            url: '/',
+			templateUrl: './html/chart1/chart1.html',
+			controller: 'chart1Controller'
+        })
+		.state('test', {
+            url: '/test',
+			templateUrl: './html/chart1/chart1.html',
+			controller: 'chart1Controller'
+		});
+}
--- server/config/passport.js
+++ server/config/passport.js
@@ -1,27 +1,6 @@
-import { Strategy as FamilySearchStrategy } from 'passport-familysearch';
+
 
 module.exports = passport => {
 
-	passport.serializeUser((user, done) => {
-		done(null, user);
-	});
-
-	passport.deserializeUser((obj, done) => {
-		done(null, obj);
-	});
-
-	passport.use(new FamilySearchStrategy({
-		authorizationURL: 'https://sandbox.familysearch.org/cis-web/oauth2/v3/authorization',
-		tokenURL: 'https://sandbox.familysearch.org/cis-web/oauth2/v3/token',
-		devKey: "a02j000000BpvVCAAZ",
-		callbackURL: "http://localhost:3000/auth/familysearch/callback",
-		environment: 'sandbox'
-	}, (accessToken, refreshToken, profile, done) => {
-
-		// import users and stuff here
-
-		console.log("access token:", accessToken);
-		console.log("profile", profile);
-		return done(null, profile);
-	}));
+	
 };

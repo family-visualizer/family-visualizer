c8ddea125ab4504fc9543935f7a5a574e8111347
Erin Sheridan <SheridanErin@gmail.com>
2016-01-07 17:04:47 -0700

    deleted unneccessary infoBoxCtrl

--- main/client/features/infoBoxes/infoBoxDir.js
+++ main/client/features/infoBoxes/infoBoxDir.js
@@ -3,7 +3,6 @@ angular.module('app').directive('infoBoxDir', function () {
 	return {
 		  restrict: "E"
 		, templateUrl: "./dist/html/infoBoxes/infoBoxTmp.html"
-		, controller: "infoBoxCtrl"
 		, scope: {
 			  infoBox: '='
 		}

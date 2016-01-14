d91ac58a4245b899ded873f906a307952280ffa6
lukeschunk <lukeschunk@gmail.com>
2016-01-13 22:42:23 -0700

    got hover functionality working

--- main/client/features/chart-birth-month/birth-month-chart-dir.js
+++ main/client/features/chart-birth-month/birth-month-chart-dir.js
@@ -6,149 +6,63 @@ angular.module('app').directive('birthMonth', () => {
         controller: 'birthMonthCtrl',
         link(scope, elem, attrs) {
 
-            var generationArray =
-			[
-				{
-					generation: 1,
-					data:
-					[
-						{
-							age: "January",
-							population: 6
-						},
-						{
-							age: "February",
-							population: 8
-						},
-						{
-							age: "March",
-							population: 3
-						}
-					]
-				},
-				{
-					generation: 2,
-					data:
-					[
-						{
-							age: "January",
-							population: 14
-						},
-						{
-							age: "February",
-							population: 0
-						},
-						{
-							age: "March",
-							population: 8
-						}
-					]
-				},
-				{
-					generation: 3,
-					data:
-					[
-						{
-							age: "January",
-							population: 12
-						},
-						{
-							age: "February",
-							population: 12
-						},
-						{
-							age: "March",
-							population: 8
-						}
-					]
-				}
+			var dataset = {
+			  January: [12, 10, 10, 19, 10],
+			  February: [11, 15, 19, 10, 10],
+			  March: [10, 0, 10, 10, 18],
+			  April: [10, 15, 10, 10, 10],
+			  May: [10, 15, 11, 102, 19],
+			};
 
-			];
+            var width = 860,
+                height = 900,
+                cwidth = 60;
 
-			passInGenerations(generationArray);
+            var color = d3.scale.category20();
 
-			function passInGenerations(generationArray) {
-				for(var i = 0; i < generationArray.length; i++) {
-					console.log("this is generationArray[i]", generationArray[i]);
-					// console.log("this is generationArray[i][data]", generationArray[i][data]);
-					// console.log("this is generationArray[i][generation]", generationArray[i][generation])
-					generateDonut(generationArray[i].data, generationArray[i].generation);
-				}
+            var pie = d3.layout.pie()
+                .sort(null);
+
+            var arc = d3.svg.arc();
+
+            var svg = d3.select("body").append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .append("g")
+                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+
+            var gs = svg.selectAll("g").data(d3.values(dataset)).enter().append("g");
+            var path = gs.selectAll("path")
+                .data(function(d) {
+                    return pie(d);
+                })
+                .enter().append("path")
+				.attr("class", "test-baby")
+                .attr("fill", function(d, i) {
+                    return color(i);
+                })
+                .attr("d", function(d, i, j) {
+                    return arc.innerRadius(10 + cwidth * j).outerRadius(cwidth * (j + 1))(d);
+                });
+
+			d3.selectAll("path")
+				.on("mouseover", darken)
+				.on("mouseleave", lighten);
+
+			d3.selectAll("path")
+				.style("opacity", 0.2);
+
+			function darken() {
+				d3.select(this)
+					.style("opacity", 0.8);
+			};
+
+			function lighten() {
+				d3.select(this)
+					.style("opacity", 0.2);
 			}
-			// var test = [
-			// 	{
-			// 		age: "January",
-			// 		population: 6
-			// 	},
-			// 	{
-			// 		age: "February",
-			// 		population: 8
-			// 	},
-			// 	{
-			// 		age: "March",
-			// 		population: 4
-			// 	}
-			// ]
 
-			// generateDonut(test, 3);
-
-            function generateDonut(data, genMultiplier) {
-				console.log("this is data", data);
-				console.log("this is genMultiplier", genMultiplier);
-                var width = 300,
-                    height = 150,
-                    radius = (Math.min(width, height) / 2) * genMultiplier;
-
-                var color = d3.scale.ordinal()
-                    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-                var arc = d3.svg.arc()
-                    .outerRadius(80 - 10)
-                    .innerRadius(80 - 70);
-
-                var pie = d3.layout.pie()
-                    .sort(null)
-                    .value(function(d) {
-                        return d.population;
-                    });
-
-                var svg = d3.select("body").append("svg")
-                    .attr("width", width)
-                    .attr("height", height)
-                    .append("g")
-                    .attr("transform", "translate(" + 30 + "," + 90 + ")");
-
-
-                console.log("this is daXXXta", data);
-
-
-                var g = svg.selectAll(".arc")
-                    .data(pie(data))
-                    .enter().append("g")
-                    .attr("class", "arc");
-
-                g.append("path")
-                    .attr("d", arc)
-                    .style("fill", function(d) {
-                        console.log("this is d", d);
-                        return color(d.data.age);
-                    });
-
-                g.append("text")
-                    .attr("transform", function(d) {
-                        return "translate(" + arc.centroid(d) + ")";
-                    })
-                    .attr("dy", ".35em")
-                    .text(function(d) {
-                        return d.data.age;
-                    });
-
-
-                function type(d) {
-                    d.population = +d.population;
-                    return d;
-                }
-            }
         }
     }
 

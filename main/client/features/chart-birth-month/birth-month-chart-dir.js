d2ce07fe9fc40a4ebf6d13f95102aa0b9c463bcf
lukeschunk <lukeschunk@gmail.com>
2016-01-16 14:07:03 -0700

    got clicking adding new class

--- main/client/features/chart-birth-month/birth-month-chart-dir.js
+++ main/client/features/chart-birth-month/birth-month-chart-dir.js
@@ -19,12 +19,13 @@ angular.module('app').directive('birthMonth', () => {
                 height = 900,
                 cwidth = 45;
 
-			console.log("d3.scale", d3.scale);
+
 			var color = d3.scale.ordinal()
     .range(["#01A2A6", "#29D9C2", "#BDF271", "#5E0042", "#005869", "#00856A", "#8DB500", "#C22121", "#047878", "#F57336", "#FFB733", "#8D1C96"]);
 
             var pie = d3.layout.pie()
                 .sort(null);
+
 
             var arc = d3.svg.arc();
 
@@ -37,15 +38,22 @@ angular.module('app').directive('birthMonth', () => {
 
             var gs = svg.selectAll("g").data(d3.values(dataset)).enter().append("g");
 
+			console.log("this is gs", gs);
+
             var path = gs.selectAll("path")
                 .data(function(d) {
                     return pie(d);
                 })
                 .enter().append("path")
-				.attr("class", "test-baby")
                 .attr("fill", function(d, i) {
                     return color(i);
                 })
+				.attr("class", function(d, i) {
+					return "class" + i;
+				})
+				.on('click', function(d, i) {
+					svg.selectAll(".class" + i).classed("selected", true)
+				})
                 .attr("d", function(d, i, j) {
                     return arc.innerRadius(10 + cwidth * j).outerRadius(cwidth * (j + 1))(d);
                 });

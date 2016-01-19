745129235b331692eb40109e61559e0b4e896b51
Erin Meyers <erinmeyers88@gmail.com>
2016-01-19 13:54:10 -0700

    working on menu

--- main/client/features/menuBar/menuBarCtrl.js
+++ main/client/features/menuBar/menuBarCtrl.js
@@ -1,6 +1,10 @@
-angular.module('app').controller('menuBarCtrl', function ($scope, ngDialog, $state) {
+angular.module('app').controller('menuBarCtrl', function ($scope, ngDialog, $state, $location) {
 
 	$scope.openLoginModal = function () {
+		
+		$scope.showLogin = false;
+		$scope.showLogout = true;
+		
 		ngDialog.open({
 			template: './dist/html/menuBar/loginModalTmp.html'
 			, scope: $scope
@@ -11,5 +15,20 @@ angular.module('app').controller('menuBarCtrl', function ($scope, ngDialog, $sta
 		ngDialog.closeAll();
 	};
 
+
+	$scope.url = $location.url();
+
+	$scope.showLogin = function () {
+		if ($scope.url !== "/login") {
+			return false;
+		} else {
+			return true;
+		}
+	};
+
+	$scope.showLogout = false;
+	
+	$scope.showLogin = true;
+	
 });
 

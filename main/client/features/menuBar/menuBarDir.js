6d8aca9e2c6d112123b655229c73c15470309e1c
Erin Meyers <erinmeyers88@gmail.com>
2016-01-20 16:58:00 -0700

    styling

--- main/client/features/menuBar/menuBarDir.js
+++ main/client/features/menuBar/menuBarDir.js
@@ -2,7 +2,7 @@ angular.module('app').directive('menuBarDir', function ($location) {
 
 	return {
 		  restrict: "E"
-		, templateUrl: "./dist/html/menuBar/menuBarTmp.html"
+		, templateUrl: "./dist/html/menuBar/demoMenuBarTmp.html"
 		, controller: "menuBarCtrl"
 		, scope: {
 			user: '='
